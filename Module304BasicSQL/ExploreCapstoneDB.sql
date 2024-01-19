fca41d49e8ca311fd0aa819b26418573d9eb9e36
topherjn <nicholsonc@gmail.com>
2024-01-19 12:37:36 -0500

    Created db exploration file and added the first few describe commands on tables

---
+++ Module304BasicSQL/ExploreCapstoneDB.sql
@@ -0,0 +1,31 @@
+use capstone;
+
+show tables;
+-- events
+-- locations
+-- persons
+
+-- So far it looks like the database holds data on individuals
+-- who go missing, where they went missing, and when. 
+-- Every table has an auto-increment primary key, and
+-- the events table is a junction table between the persons
+-- table and the location table
+
+-- "YES" and "NO" represent whether or not a column in nullable
+
+describe events;
+-- event_id	int	NO	PRI		auto_increment
+-- person_id	int	YES	MUL		
+-- location_id	int	YES	MUL		
+-- event_date	date	YES
+
+describe locations;	
+-- event_id	int	NO	PRI		auto_increment
+-- person_id	int	YES	MUL		
+-- location_id	int	YES	MUL		
+-- event_date	date	YES		
+
+describe persons;		
+-- person_id	int	NO	PRI		auto_increment
+-- name	text	NO			
+-- status	text	YES				
