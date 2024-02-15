5ec812786c8819fa5560e737d00d4674143ee986
topherjn <nicholsonc@gmail.com>
2024-02-15 10:52:57 -0500

    leading zeroes in branch zips fixed

--- cdw_data_reader.py
+++ cdw_data_reader.py
@@ -1,12 +1,13 @@
-import json
 import pandas as pd
+import numpy as np
 
 def get_dataframe(data_file):
 
     data_folder = 'data'
 
+    # one JSON object per line in file
     df = pd.read_json(f"{data_folder}/{data_file}", lines=True)
-
+    
     return df
 
 if __name__ == "__main__":
@@ -20,4 +21,8 @@ if __name__ == "__main__":
     credit_df = get_dataframe(CREDIT_FILE)
     customer_df = get_dataframe(CUSTOMER_FILE)
 
-    print(branch_df['BRANCH_ZIP'])
+#https://stackoverflow.com/questions/33137686/python-loading-zip-codes-into-a-dataframe-as-strings
+branch_df['BRANCH_ZIP'] = branch_df['BRANCH_ZIP'].astype(str).str.zfill(5)
+
+print(branch_df)
+
