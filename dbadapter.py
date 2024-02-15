4196b3e2e671c9d0c4ffe3a5a69fddc5fb514e2c
topherjn <nicholsonc@gmail.com>
2024-02-15 17:03:50 -0500

    Stubbed out data adapter queries

--- dbadapter.py
+++ dbadapter.py
@@ -39,6 +39,29 @@ class DataAdapter:
         cursor.execute(command)
         results = cursor.fetchall()
         return results
-    
+
+    # 2.1.3- Use the provided inputs to query the database and retrieve a list of transactions made by customers in the specified zip code for the given month and year.
+    # 2.1.4 - Sort the transactions by day in descending order.    
     def get_specified_transactions(self,zip_code, month, year):
         return zip_code, month, year
+    
+    # 1) Used to check the existing account details of a customer.
+    def get_customer_details(self,ssn):
+        pass
+
+    # 2) Used to modify the existing account details of a customer.
+    # get all details in a data object, change, then save whole thing back
+    def update_customer_details(self,snn):
+        pass
+
+    # 3) Used to generate a monthly bill for a credit card number for a given month and year. 
+    # Hint: What does YOUR monthly credit card bill look like?  What structural components 
+    # does it have?  Not just a total $ for the month, right?
+
+    def generate_customer_bill(self, snn):
+        pass
+
+    # 4) Used to display the transactions made by a customer between two dates.
+    # Order by year, month, and day in descending order.
+    def generate_transaction_report(self, snn, start, end):
+        pass
--- menu.py
+++ menu.py
@@ -57,21 +57,18 @@ def do_menu():
 
     data_adapter = DataAdapter()
 
+    # 2.1.3- Use the provided inputs to query the database and retrieve a list of transactions made by customers in the specified zip code for the given month and year.
+    # 2.1.4 - Sort the transactions by day in descending order.
     z,m,y = data_adapter.get_specified_transactions(zip_code,month,year)
 
     print(z,m,y)
 
-
-
-    # 2.1.3- Use the provided inputs to query the database and retrieve a list of transactions made by customers in the specified zip code for the given month and year.
-
-
-    # 2.1.4 - Sort the transactions by day in descending order.
-
     # Remember: this function should be callable from the main application interface and the output should be screen-reading friendly for the user.
     # 2)    Used to display the number and total values of transactions for a given type.
+    """This can be done post query"""
     # 3)    Used to display the total number and total values of transactions for branches in a given state.
-
+    """This can be done post query"""
+    
     # 2.2 Customer Details Module
 
     # Req-2.2
