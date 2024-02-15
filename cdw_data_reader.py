e1554d00f2a60b8261889330329810ee85ac9b16
topherjn <nicholsonc@gmail.com>
2024-02-15 02:52:11 -0500

    dataframe reader works

--- cdw_data_reader.py
+++ cdw_data_reader.py
@@ -1,25 +1,23 @@
 import json
 import pandas as pd
 
- 
+def get_dataframe(data_file):
 
-"""
-The CDW files are not in a true JSON format. They are more
-files with a python dictionary on each line, missing the initial
-key for each dictionary.  This module started out as my trying
-just to read ZIPs to make sure they're not +9, where I noticed
-the lack of compliance to JSON.
+    data_folder = 'data'
 
-This module forces each line into a JSON format and then creates
-a list of data items and returns that list to the caller.
-"""
+    df = pd.read_json(f"{data_folder}/{data_file}", lines=True)
 
-# Input: CDW JSON filename
-# Returns: List of dictionaries containting JSON objects
+    return df
 
-data_folder = 'data'
-data_file = 'cdw_sapp_custmer.json'
+if __name__ == "__main__":
 
-df = pd.read_json(f"{data_folder}/{data_file}", lines=True)
+    # make files names easier
+    BRANCH_FILE = "cdw_sapp_branch.json"
+    CREDIT_FILE = "cdw_sapp_branch.json"
+    CUSTOMER_FILE = "cdw_sapp_custmer.json"
 
-print(df)
+    branch_df = get_dataframe(BRANCH_FILE)
+    credit_df = get_dataframe(CREDIT_FILE)
+    customer_df = get_dataframe(CUSTOMER_FILE)
+
+    print(branch_df['BRANCH_ZIP'])
