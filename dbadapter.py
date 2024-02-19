13551ef2c5f0d361e654faf09b99d6ec7e558561
topherjn <nicholsonc@gmail.com>
2024-02-19 10:17:15 -0500

    stubbed out and commented out dataadapter

--- dbadapter.py
+++ dbadapter.py
@@ -11,19 +11,21 @@ CUSTOMER_TABLE = "CDW_SAPP_CUSTOMER"
 
 
 class DataAdapter:
-    def __init__(self):
-        self.connection = \
-            mysql.connector.connect(
-                host="localhost",           
-                user=secrets.mysql_username,
-                password=secrets.mysql_password)
-        self.database_name = DATABASE_NAME
+    # def __init__(self):
+    #     self.connection = \
+    #         mysql.connector.connect(
+    #             host="localhost",           
+    #             user=secrets.mysql_username,
+    #             password=secrets.mysql_password)
+    #     self.database_name = DATABASE_NAME
+    pass
 
     def create_database(self):
-        cursor = self.connection.cursor(buffered=True)
-        command = f"CREATE DATABASE IF NOT EXISTS {self.database_name}"
-        cursor.execute(command)
-        cursor.close()
+        # cursor = self.connection.cursor(buffered=True)
+        # command = f"CREATE DATABASE IF NOT EXISTS {self.database_name}"
+        # cursor.execute(command)
+        # cursor.close()
+        pass
 
     def create_tables(self):
         # create customers table
@@ -34,12 +36,12 @@ class DataAdapter:
         pass
 
     def get_all_customers(self):
-        command = f"SELECT * FROM {CUSTOMER_TABLE}"
-        cursor = self.connection.cursor(buffered=True)
-        cursor.execute(command)
-        results = cursor.fetchall()
+        # command = f"SELECT * FROM {CUSTOMER_TABLE}"
+        # cursor = self.connection.cursor(buffered=True)
+        # cursor.execute(command)
+        # results = cursor.fetchall()
         
-        return results
+        pass
 
     # 2.1.3- Use the provided inputs to query the database and retrieve a list of transactions made by customers in the specified zip code for the given month and year.
     # 2.1.4 - Sort the transactions by day in descending order.    
@@ -53,8 +55,8 @@ class DataAdapter:
     # 2) Used to modify the existing account details of a customer.
     # get all details in a data object, change, then save whole thing back
     def update_customer_details(self,ssn):
-        details = self.get_customer_details(self, ssn)
-
+        # details = self.get_customer_details(self, ssn)
+        pass
     # 3) Used to generate a monthly bill for a credit card number for a given month and year. 
     # Hint: What does YOUR monthly credit card bill look like?  What structural components 
     # does it have?  Not just a total $ for the month, right?
