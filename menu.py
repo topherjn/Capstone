3404211f5e91da392b6d6d498440f3b6bc0a4389
topherjn <nicholsonc@gmail.com>
2024-02-21 10:38:38 -0500

    added some comments

--- menu.py
+++ menu.py
@@ -7,13 +7,14 @@ import dbadapter as db
 findspark.init()
 
 
+# make clear screen platform agnostic-ish
 def clear_screen():
     if name == 'nt':
         system('cls')
     else:
         system('clear')
 
-
+# restrict user input to integers
 def get_integer(prompt):
     value = input(prompt)
     try:
@@ -28,6 +29,8 @@ def do_menu():
 
     # 2.1.1 - Prompt the user for a zip code, provide contextual cues for valid input, and verify it is in the
     # correct format.
+
+    # consider getting a list of zip from build_database()
     clear_screen()
     zip_code = input("Enter a 5-digit ZIP code: ")
 
@@ -36,6 +39,8 @@ def do_menu():
 
     # 2.1.2- Ask for a month and year,  and provide contextual cues for valid input and verify it is in the correct
     # format.
+        
+    # present months menu
     clear_screen()
     print(f"Querying on ZIP code {zip_code}: ")
     months = list(cal.month_name)
@@ -43,13 +48,16 @@ def do_menu():
         if i > 0:
             print(i, m)
 
+    # check user month input
     month = get_integer("Enter the menu number for the month you want to query: ")
     while month not in range(1, 13):
         month = int(input("Invalid month.  Enter the menu number for the month you want to query: "))
 
+    # get the year from user
     clear_screen()
     year = get_integer(f"{months[month]} of which year? ")
 
+    # restrict year to 2018
     while year != 2018:
         print("No transaction data for that year.  Try again: ")
         year = get_integer(f"{months[month]} of which year? ")
