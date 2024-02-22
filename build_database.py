f265d249dd55f4b6a9e74d907e705560e122f624
topherjn <nicholsonc@gmail.com>
2024-02-22 18:51:50 -0500

    branch phone cleaned

--- build_database.py
+++ build_database.py
@@ -1,6 +1,6 @@
 import constants as const
 from dbadapter import DataAdapter
-from pyspark.sql.functions import col, lower, concat, lit, initcap, lpad
+from pyspark.sql.functions import col, lower, concat, lit, initcap, lpad, regexp_replace
 from pyspark.sql.types import StringType
 import cdw_data_reader as cdr
 import load_loan_data as lld
@@ -18,7 +18,7 @@ def build_database():
 
     # # create tables
     # customers
-    print("Creating customers table ...")
+    print("Cleaning customers data ...")
     cust_df = cdr.get_dataframe(const.CUSTOMER_FILE)
     # convert middle name to lower case
     cust_df = cust_df.withColumn("MIDDLE_NAME",lower(col("MIDDLE_NAME")))
@@ -35,27 +35,26 @@ def build_database():
                                  concat(lit("(000)"),
                                  col("CUST_PHONE")[0:3],
                                  lit("-"),
-                                 col("CUST_PHONE")[4:8]))
+                                 col("CUST_PHONE")[4:7]))
     # pad zip
     cust_df = cust_df.withColumn("CUST_ZIP",lpad("CUST_ZIP",5,"0"))
     
     # # branches
-    print("Creating branches table ...")
+    print("Cleaning branches data ...")
     branch_df = cdr.get_dataframe(const.BRANCH_FILE)
 
     # handle zip nulls with default 99999
     branch_df = branch_df.fillna(99999,subset=['BRANCH_ZIP'])
 
     # Change the format of phone number to (XXX)XXX-XXXX
-    branch_df = branch_df.withColumn("BRANCH_PHONE",
-                                 concat(lit("(000)"),
-                                 col("BRANCH_PHONE")[0:3],
-                                 lit("-"),
-                                 col("BRANCH_PHONE")[4:8]))
+    branch_df = branch_df.withColumn("BRANCH_PHONE",branch_df["BRANCH_PHONE"].cast(StringType()))
+    branch_df = branch_df.withColumn("BRANCH_PHONE",regexp_replace("BRANCH_PHONE", "(\\d{3})(\\d{3})(\\d{4})", "\($1\)$2-$3"))
+    # pad zip
+    branch_df = branch_df.withColumn("BRANCH_ZIP",lpad("BRANCH_ZIP",5,"0"))
     
 
     # # transactions
-    print("Creating transactions table ...")
+    print("Cleaning transactions data ...")
     transactions_df = cdr.get_dataframe(const.CREDIT_FILE)
 
     # pad month and day
@@ -64,15 +63,16 @@ def build_database():
 
 
     # online json
-    # print("Creating loan application table ...")
-    # loan_json_data = lld.main_request(const.LOAN_URL)
-    # df = cdr.get_dataframe(str(loan_json_data), False)
+    print("Cleaning loan application data ...")
+    loan_json_data = lld.main_request(const.LOAN_URL)
+    loan_df = cdr.get_dataframe(str(loan_json_data), False)
    
     # create the tables in MySQL
+    print("Creating tables in MySQL RDBMS")
     data_adapter.create_table(cust_df,const.CUSTOMER_TABLE)
     data_adapter.create_table(branch_df,const.BRANCH_TABLE)
     data_adapter.create_table(transactions_df,const.CC_TABLE)
-     # data_adapter.create_table(df, const.LOAN_TABLE)
+    data_adapter.create_table(loan_df, const.LOAN_TABLE)
 
     data_adapter.close()
 
