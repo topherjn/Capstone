a738be7b8cd9cba334a9e6ce472062fa91327ca9
topherjn <nicholsonc@gmail.com>
2024-02-22 15:15:04 -0500

    cust phone number altered

--- build_database.py
+++ build_database.py
@@ -4,6 +4,8 @@ from pyspark.sql.functions import col
 from pyspark.sql.functions import lower
 from pyspark.sql.functions import concat
 from pyspark.sql.functions import lit
+from pyspark.sql.functions import initcap
+from pyspark.sql.types import StringType
 import cdw_data_reader as cdr
 import load_loan_data as lld
 
@@ -21,28 +23,43 @@ def build_database():
     # # create tables
     # customers
     print("Creating customers table ...")
-    df = cdr.get_dataframe(const.CUSTOMER_FILE)
-    df = df.withColumn("MIDDLE_NAME",lower(col("MIDDLE_NAME")))
-    df = df.withColumn("APT_NO", concat(col("APT_NO"), lit(","), col("STREET_NAME")))
-    df = df.withColumnRenamed("APT_NO","FULL_STREET_ADDRESS")
-    df.drop("STREET_NAME")
-    data_adapter.create_table(df,const.CUSTOMER_TABLE)
-
+    cust_df = cdr.get_dataframe(const.CUSTOMER_FILE)
+    # convert middle name to lower case
+    cust_df = cust_df.withColumn("MIDDLE_NAME",lower(col("MIDDLE_NAME")))
+    # Concatenate Apartment no and Street name of customer's Residence with comma as a seperator (Street, Apartment)
+    cust_df = cust_df.withColumn("APT_NO", concat(col("APT_NO"), lit(","), col("STREET_NAME")))
+    cust_df = cust_df.withColumnRenamed("APT_NO","FULL_STREET_ADDRESS")
+    cust_df.drop("STREET_NAME")
+    # Convert the First and Last Name to Title Case
+    cust_df = cust_df.withColumn("FIRST_NAME",initcap(col("FIRST_NAME")))
+    cust_df = cust_df.withColumn("LAST_NAME",initcap(col("LAST_NAME")))
+    # Change the format of phone number to (XXX)XXX-XXXX
+    cust_df = cust_df.withColumn("CUST_PHONE",cust_df["CUST_PHONE"].cast(StringType()))
+    cust_df = cust_df.withColumn("CUST_PHONE",
+                                 concat(lit("(000)"),
+                                 col("CUST_PHONE")[0:3],
+                                 lit("-"),
+                                 col("CUST_PHONE")[4:8]))
     # # branches
     print("Creating branches table ...")
-    df = cdr.get_dataframe(const.BRANCH_FILE)
-    data_adapter.create_table(df,const.BRANCH_TABLE)
+    branch_df = cdr.get_dataframe(const.BRANCH_FILE)
+    
 
     # # transactions
     print("Creating transactions table ...")
-    df = cdr.get_dataframe(const.CREDIT_FILE)
-    data_adapter.create_table(df,const.CC_TABLE)
+    transactions_df = cdr.get_dataframe(const.CREDIT_FILE)
+   
 
     # online json
     # print("Creating loan application table ...")
     # loan_json_data = lld.main_request(const.LOAN_URL)
     # df = cdr.get_dataframe(str(loan_json_data), False)
-    # data_adapter.create_table(df, const.LOAN_TABLE)
+   
+    # create the tables in MySQL
+    data_adapter.create_table(cust_df,const.CUSTOMER_TABLE)
+    data_adapter.create_table(branch_df,const.BRANCH_TABLE)
+    data_adapter.create_table(transactions_df,const.CC_TABLE)
+     # data_adapter.create_table(df, const.LOAN_TABLE)
 
     data_adapter.close()
 
