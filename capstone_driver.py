2aa4a4e3cf7b67806ff03c98b3f74c11bfb9d29e
topherjn <nicholsonc@gmail.com>
2024-02-22 22:35:00 -0500

    added comments to driver

--- capstone_driver.py
+++ capstone_driver.py
@@ -1,5 +1,8 @@
 from build_database import build_database
 from menu import do_menu
 
+'''read in the data and build that MySQL DB from that data first in build_database'''
 build_database()
+
+'''do_menu() will control the flow of the user interactions after the database is built'''
 do_menu()
