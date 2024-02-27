50e10269262437f57db04229053337943a6f1086
topherjn <nicholsonc@gmail.com>
2024-02-27 17:52:39 -0500

    menu building started

--- capstone_driver.py
+++ capstone_driver.py
@@ -1,9 +1,7 @@
 from build_database import build_database
 from menu import do_menu
 
-'''Launch point for the whole program'''
 
-'''read in the data and build that MySQL DB from that data first in build_database'''
 build_database()
 
 '''do_menu() will control the flow of the user interactions after the database is built'''
--- menu.py
+++ menu.py
@@ -1,35 +1,92 @@
-from transactions_info import transactions_query
+import calendar as cal
+from os import system, name
+import findspark
+from build_database import build_database
+import dbadapter as db
+
+findspark.init()
+
+# make clear screen platform agnostic-ish
+def clear_screen():
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
+
+# restrict user input to integers
+def get_integer(prompt):
+    value = input(prompt)
+    try:
+        return int(value)
+    except ValueError:
+        print(f"{value} invalid: Try again.")
+        return get_integer(prompt)
+
+
+def transactions_query():
+    # Req-2.1
+
+    # 2.1.1 - Prompt the user for a zip code, provide contextual cues for valid input, and verify it is in the
+    # correct format.
+
+    # consider getting a list of zip from build_database()
+    clear_screen()
+    zip_code = input("Enter a 5-digit ZIP code: ")
+
+    while len(zip_code) != 5 or not zip_code.isnumeric():
+        zip_code = input("Incorrect format. Enter a 5-digit ZIP code: ")
+
+    # 2.1.2- Ask for a month and year,  and provide contextual cues for valid input and verify it is in the correct
+    # format.
+        
+    # present months menu
+    clear_screen()
+    print(f"Querying on ZIP code {zip_code}: ")
+    months = list(cal.month_name)
+    for i, m in enumerate(months):
+        if i > 0:
+            print(i, m)
+
+    # check user month input
+    month = get_integer("Enter the menu number for the month you want to query: ")
+    while month not in range(1, 13):
+        month = int(input("Invalid month.  Enter the menu number for the month you want to query: "))
+        
+    # get the year from user
+    clear_screen()
+    year = get_integer(f"{months[month]} of which year? ")
+
+    # restrict year to 2018
+    while year != 2018:
+        print("No transaction data for that year.  Try again: ")
+        year = get_integer(f"{months[month]} of which year? ")
+
+    # all dataabase operations are performed by this object
+    data_adapter = db.DataAdapter()
+
+    data_adapter.get_specified_transactions(zip_code, month, year)
+
+    data_adapter.close()
+
 
 def do_menu():
 
-    # ask user the task he wishes to perform
-    # call the code for the task requested
+    print("Tasks Menu")
 
-    
-    # 2.1.3- Use the provided inputs to query the database and retrieve a list of transactions made by customers in
-    # the specified zip code for the given month and year. 2.1.4 - Sort the transactions by day in descending order.
-    # z,m,y = data_adapter.get_specified_transactions(zip_code,month,year)
+    option = None
 
-    '''done'''
-    transactions_query()
+    while not option==0:
+        option = get_integer("Type a number to perform one of the following tasks, 0 to exit: ")
 
-    # Remember: this function should be callable from the main application interface and the output should be
-    # screen-reading friendly for the user. 
-    # 2)    Used to display the number and total values of transactions for a
-    # given type.
-    """This can be done post query"""
-    # 3)    Used to display the total number and total values of transactions for branches in a given state.
-    """This can be done post query"""
+        if option == 1:
+            transactions_query()
 
-    # 2.2 Customer Details Module
-
-    # Req-2.2
-
-    # Customer Details
-   
-
-    
-
+    '''we need a loop that offers all the options
+       keep asking for all the options until exit
+       request
+       create a function for each task above? probably
+       todo next - get the list of tasks bn 
+    '''
 
 if __name__ == "__main__":
     do_menu()
